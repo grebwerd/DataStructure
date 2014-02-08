e86c3d69495cc19d830994fd30165ec57a2a3785
grebwerd <grebwerd@gmail.com>
2014-02-07 22:19:25 -0700

    Modified LinkedList.hpp, changed add( T val ) to append( T val ), added prepend( T val ), added deleteNode( T val )

--- LinkedList/LinkedList.hpp
+++ LinkedList/LinkedList.hpp
@@ -6,42 +6,97 @@ template <class T>
 class LinkedList 
 {
     public:
-        LinkedList() : head ( NULL ){}
+        LinkedList() : headNode ( NULL ){}
 
-        void insert( T val )
+        void append( T val )
         {
             Node<T>* tempNode;
-            if( NULL == head )
+            if( NULL == headNode )
             {
-                head = new Node<T>();
-                head->nextNode = new Node<T>( val );
+                headNode = new Node<T>();
+                headNode->nextNode = new Node<T>( val );
                 return;
             }
             tempNode = new Node<T>();
-            tempNode->nextNode = head->nextNode;
+            tempNode->nextNode = headNode->nextNode;
             while( true )
             {
                 if( !tempNode->nextNode->nextNode )
                 {
-                    tempNode->nextNode->nextNode
-                    = new Node<T>( val );
+                    tempNode->nextNode->nextNode = new Node<T>( val );
                     return;
                 }
                 tempNode->nextNode = tempNode->nextNode->nextNode;
             }
         }
 
+        void deleteNode( T val )
+        {
+            if( NULL == headNode || NULL == headNode->nextNode )
+            {
+                std::cout << "LinkedList is empty " << std::endl;
+                return;
+            }
+
+            Node<T>* tempNode = new Node<T>();
+            tempNode->nextNode = headNode->nextNode;
+
+            while( tempNode->nextNode )
+            {
+                if( tempNode->nextNode->getVal() == val )
+                {
+                    if( NULL == tempNode->nextNode->nextNode )
+                    {
+                        tempNode->nextNode = NULL;
+                        return;
+                    }
+                    if( headNode->nextNode->getVal() == val )
+                    {
+                        Node<T>* x = headNode->nextNode->nextNode;
+                        delete headNode->nextNode;
+                        headNode->nextNode = x;
+                        return;
+                    }
+                    else
+                    {
+                        Node<T>* x = tempNode->nextNode->nextNode;
+                        delete tempNode->nextNode;
+                        tempNode->nextNode = x;
+                        return;
+                    }
+                }
+                tempNode = tempNode->nextNode;
+            }
+            std::cout << "The value " << val << " is not in the LinkedList " << std::endl;
+        }
+
+        void prepend( T val )
+        {
+            Node<T>* tempNode;
+            if( NULL == headNode )
+            {
+                headNode = new Node<T>();
+                headNode->nextNode = new Node<T>( val );
+            }
+            else 
+            {
+                Node<T>* newNode = new Node<T>( val );
+                newNode->nextNode = headNode->nextNode;
+                headNode->nextNode = newNode;
+            }
+        }
+
         void display() 
         {
             Node<T>* tempNode;
-            if( NULL == head )
+            if( NULL == headNode )
             {
-                std::cout<<"head ptr is null"<<std::endl;
+                std::cout << "headNode ptr is null " << std::endl;
                 return;
             }
             tempNode = new Node<T>();
-            tempNode->nextNode = head->nextNode;
-            std::cout<<"LinkedList: ";
+            tempNode->nextNode = headNode->nextNode;
+            std::cout << "LinkedList: ";
             while( tempNode->nextNode )
             {
                 std::cout<<tempNode->nextNode->getVal()<<" ";
@@ -51,7 +106,7 @@ class LinkedList
         }
 
     private:
-        Node<T>* head;
+        Node<T>* headNode;
 };
 
 
